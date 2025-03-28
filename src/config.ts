cdfa6b63e31f3f9e4e17908d974cbb048d4cac28
Daniel Emeka Obikoru <daniel.emeka@blinqpay.io>
2025-03-28 10:11:48 +0100

    feat(reactversion): Test setup

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ import { celoAlfajores } from "viem/chains";
 
 const config = getDefaultConfig({
   appName: "Alpha",
-  projectId: "",
+  projectId: "YOUR_PROJECT_ID",
   chains: [celoAlfajores],
   ssr: true, // If your dApp uses server side rendering (SSR)
 });
